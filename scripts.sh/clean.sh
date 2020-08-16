86f71471070cf83d8fec1bcb25b665e304c347da
Joseph T Lyons <JosephTLyons@gmail.com>
2020-08-16 00:18:34 -0400

    Add scripts

---
+++ scripts.sh/clean.sh
@@ -0,0 +1,5 @@
+# Script created from the commands here
+# https://www.raspberrypi.org/documentation/raspbian/updating.md
+
+# Remove downloaded package files (.deb files)
+sudo apt clean
--- scripts.sh/update.sh
+++ scripts.sh/update.sh
@@ -1,5 +1,6 @@
 # Script created from the commands here
 # https://www.raspberrypi.org/documentation/raspbian/updating.md
+# Note: This will only update for the current major version, it will not update to a newer major version
 
 # Update system's package list
 sudo apt update
