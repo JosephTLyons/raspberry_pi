17742034041f21481a0103525ded5e4693cea7e4
Joseph T Lyons <JosephTLyons@gmail.com>
2020-08-16 00:10:52 -0400

    Create update_packages.sh

---
+++ scripts.sh/update_packages.sh
@@ -0,0 +1,8 @@
+# Script created from the commands here
+# https://www.raspberrypi.org/documentation/raspbian/updating.md
+
+# Update system's package list
+sudo apt update
+
+# upgrade all installed packages to their latest versions
+sudo apt full-upgrade
