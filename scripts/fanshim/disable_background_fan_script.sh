1f52b8464443d8245d67f390720e3099092117e1
Joseph T Lyons <JosephTLyons@gmail.com>
2021-04-08 02:11:20 -0400

    Added a few scripts for fan shim

---
+++ scripts/fanshim/disable_background_fan_script.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo systemctl stop pimoroni-fanshim.service
+sudo systemctl disable pimoroni-fanshim.service
---
+++ scripts/fanshim/enable_background_fan_script.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo systemctl enable pimoroni-fanshim.service
+sudo systemctl start pimoroni-fanshim.service
